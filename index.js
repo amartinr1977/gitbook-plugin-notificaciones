19d1061eac14a5c5a3c0f4ec215380a8819570ee
amartinr1977 <amartinromero@gmail.com>
2018-02-18 22:36:28 +0100

    version 0.0.3

--- index.js
+++ index.js
@@ -13,7 +13,27 @@ module.exports = {
   blocks: {
     notificacion_info: {
       process: function (block) {
-        return "Hola " + block.body;
+        logo_info = '<img src="images/logoalerta-x80.png"></img>';
+        formato_entrega = "Esto es el formato de entrega ...";
+        requisitos = "Los requisitos van a ser ...";
+        element = '<div class="">';
+        element += '<table>';
+        element += '<tr>';
+        element += '<td rowspan="3">' + logo_info + '<br>' + block.kwargs.numejer + '</td>';
+        element += '<td>' + block.kwargs.title + '</td>';
+        element += '</tr>';
+        element += '<tr>';
+        element += '<td>' + requisitos + '</td>';
+        element += '</tr>';
+        element += '<tr>';
+        element += '<td>' + block.body + '</td>';
+        element += '</tr>';
+        element += '<tr>';
+        element += '<td colspan="2">' + formato_entrega + '</td>';
+        element += '</tr>';
+        element += '</table>';
+        element += '</div>';
+        return element;
       }
     }
   }
