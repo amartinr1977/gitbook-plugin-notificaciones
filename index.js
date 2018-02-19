0f35d50afb546588291675fe7d7e05ac0cb9c153
amartinr1977 <amartinromero@gmail.com>
2018-02-19 22:43:05 +0100

    version 0.0.7

--- index.js
+++ index.js
@@ -12,14 +12,55 @@ module.exports = {
   },
 
   blocks: {
+    notificacion_task: {
+      process: function (block) {
+        ruta_base = "https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master";
+        logo_aviso = '<img src=' + ruta_base + '"/assets/images/logobombilla-x80.png?raw=true"></img>';
+        logo_texto = 'Tarea</br>' + block.kwargs.numexer;
+
+        element = '<div class="contenedor contenedor-task">';
+        // Indicamos el logo o imagen de la notificación relativa a: Tarea
+        element += '<div class="image">' + logo_aviso + '</br><b><tt>' + logo_texto + '</tt></b></div>';
+        element += '<div class="contenedor-titulo"><div class="titulo titulo-ejercicio"><tt>' + block.kwargs.title + '</tt>';
+        element += '<div class="requisitos" style="font-size: 70%;">';
+        // Establecemos los Requisitos necesarios para realizar la tarea
+        element += '<b><i><tt>Requisitos: <span style=" color: brown;">' + block.kwargs.req + '</span>';
+        element += '</b></i></tt></div>';
+        element += '</div></div>';
+        // Agregamos el cuerpo o contenido de la tarea a realizar
+        element += '<div class="contenido">' + block.body + '</div>';
+        // Establecemos el formato de entrega del ejercicio:
+        element += '<div class="formato_entrega">';
+        element += '<tt><b>Formato de Entrega:</b> ' + block.kwargs.formatoentrega + '</tt>';
+        element += '</div></div>';
+
+        return element;
+      }
+    },
+    notificacion_didyouknow: {
+      process: function (block) {
+        ruta_base = "https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master";
+        logo_aviso = '<img src=' + ruta_base + '"/assets/images/logoaviso-x80.png?raw=true"></img>';
+        logo_texto = '¿¿Sabías qué??';
+
+        element = '<div class="contenedor">';
+        element += '<div class="image">' + logo_aviso + '</br><b><tt>' + logo_texto + '</tt></b></div>';
+        element += '<div class="contenedor-titulo"><div class="titulo titulo-aviso"><tt>' + block.kwargs.title + '</tt></div></div>';
+        element += '<div class="contenido">' + block.body + '</div>';
+        element += '</div>';
+
+        return element;
+      }
+    },
     notificacion_important: {
       process: function (block) {
         ruta_base = "https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master";
         logo_lupa = '<img src=' + ruta_base + '"/assets/images/logolupa-x80.png?raw=true"></img>';
+        logo_texto = '¡¡Importante!!';
         
         element = '<div class="contenedor">';
-        element += '<div class="image">' + logo_lupa + '</div>';
-        element += '<div class="contenedor-titulo"><div class="titulo">' + block.kwargs.title + '</div></div>';
+        element += '<div class="image">' + logo_alert + '</br><b><tt>' + logo_texto + '</tt></b></div>';
+        element += '<div class="contenedor-titulo"><div class="titulo titulo-lupa"><tt>' + block.kwargs.title + '</tt></div></div>';
         element += '<div class="contenido">' + block.body + '</div>';
         element += '</div>';
 
@@ -29,17 +70,18 @@ module.exports = {
     notificacion_alert: {
       process: function (block) {
         logo_alert = '<img src="https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master/assets/images/logoalerta-x80.png?raw=true"></img>';
-        
+        logo_texto = '¡¡Aviso!!';
+
         element = '<div class="contenedor">';
-        element += '<div class="image">' + logo_alert + '</div>';
-        element += '<div class="contenedor-titulo"><div class="titulo">' + block.kwargs.title + '</div></div>';
+        element += '<div class="image">' + logo_alert + '</br><b><tt>' + logo_texto + '</tt></b></div>';
+        element += '<div class="contenedor-titulo"><div class="titulo titulo-alert"><tt>' + block.kwargs.title + '</tt></div></div>';
         element += '<div class="contenido">' + block.body + '</div>';
         element += '</div>';
 
         return element;
       }
     },
-    notificacion_info: {
+    notificacion_table: {
       process: function (block) {
         logo_info = '<img src="https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master/assets/images/logoalerta-x80.png?raw=true"></img>';
         formato_entrega = "Esto es el formato de entrega ...";
